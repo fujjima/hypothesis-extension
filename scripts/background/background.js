b7ba7f82eebbd1dc8f1c0169dacb58f8954693c2
katsuimasato <masato.lond2@outlook.jp>
2023-10-05 00:08:12 +0900

    Setting base component

--- scripts/background/background.js
+++ scripts/background/background.js
@@ -1,8 +1,9 @@
 function reddenPage() {
-  document.body.style.backgroundColor = 'red';
+  document.body.style.backgroundColor = 'blue';
 }
 
-// ref) https://developer.chrome.com/docs/extensions/reference/scripting/
+// executeScriptから呼び出した関数内での他関数の実行について
+//  ref) https://developer.chrome.com/docs/extensions/reference/scripting/#method-executeScript:~:text=a%20javascript%20function%20to%20inject.%20this%20function%20will%20be%20serialized%2C%20
 chrome.action.onClicked.addListener((tab) => {
   if (!tab.url.includes('chrome://')) {
     chrome.scripting.executeScript({
--- scripts/content/content.js
+++ scripts/content/content.js
@@ -1,9 +1,15 @@
-window.addEventListener('load', () => {
-  let targetElement = document.querySelector('#blog-title-content');
+import { format } from 'date-fns'
 
-  if (targetElement) {
-    console.log('find element');
-  } else {
-    console.log('not find element');
-  }
+import { test } from '../../func-test.js'
+
+window.addEventListener('load', async () => {
+  test();
+  console.log(format(new Date(), "'Today is a' eeee"))
+  // let targetElement = document.querySelector('#blog-title-content');
+
+  // if (targetElement) {
+  //   console.log('find element');
+  // } else {
+  //   console.log('not find element');
+  // }
 });
